f59098f8fea017850a00878bd4eaaf5e58e68ecb
Anton Vitushko <vitushko.anton@gmail.com>
2022-05-30 21:23:41 +0300

    My solution homework 5.2 v0.1

--- cache.go
+++ cache.go
@@ -1,23 +1,69 @@
 package cache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
+
+type Data struct {
+	value    string
+	deadline *time.Time
+}
 
 type Cache struct {
+	mut  sync.Mutex
+	data map[string]Data
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		data: map[string]Data{},
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (cach *Cache) Get(key string) (string, bool) {
+	cach.mut.Lock()
+	defer cach.mut.Unlock()
 
+	if _, ok := cach.data[key]; !ok {
+		return "", false
+	}
+
+	if cach.data[key].deadline != nil && cach.data[key].deadline.Before(time.Now()) {
+		return "", false
+	}
+
+	return cach.data[key].value, true
 }
 
-func (receiver) Put(key, value string) {
+func (cach *Cache) Put(key, value string) {
+	cach.mut.Lock()
+	defer cach.mut.Unlock()
+
+	cach.data[key] = Data{value, nil}
 }
 
-func (receiver) Keys() []string {
+func (cach *Cache) Keys() []string {
+	cach.mut.Lock()
+	defer cach.mut.Unlock()
+
+	var keys []string
+	now := time.Now()
+
+	for k, v := range cach.data {
+		if v.deadline != nil && v.deadline.Before(now) {
+			continue
+		}
+
+		keys = append(keys, k)
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cach *Cache) PutTill(key, value string, deadline time.Time) {
+	cach.mut.Lock()
+	defer cach.mut.Unlock()
+
+	cach.data[key] = Data{value, &deadline}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-united-school-homework-5.2
+
+go 1.17
